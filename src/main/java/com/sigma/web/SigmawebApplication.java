dd191046c9845bc18455de585e3533ce0bfb3161
aryem <aryemfedorov@gmail.com>
2023-03-25 18:48:50 +0200

    Initial commit

---
+++ src/main/java/com/sigma/web/SigmawebApplication.java
@@ -0,0 +1,16 @@
+package com.sigma.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class SigmawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmawebApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/sigma/web/SigmawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
